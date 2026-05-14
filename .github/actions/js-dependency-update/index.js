9a4323aa4df107c205c27a6c1ca9632b71f90a72
Robin Snelson <rsnelson@heliostech.co.uk>
2026-05-14 19:02:45 +0100

    feat(17 - 2 - Custom Actions - JS) add js action and workflow

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,6 +1,18 @@
 const core = require("@actions/core");
 
 async function run() {
+  /* 1.  Parse inputs:
+        1.1 base-branch from which to check for updates
+        1.2 target-branch to use to create the Parse
+        1.3 Github Token for authenticatiuon purposes (to create PSs)
+        1.4 Working Directory for which to checkj for dependencies
+    2. Execute the NPM Update the command within the working directory
+    3. Check whether modified *package.json
+    4. If there are Modified files
+        4.1 Add modified files to the target branch
+        4.2 create a PR to the base branch using the Octokit API
+    5. Otherwise, conclude the custom action
+    */
   core.info("I am a custom JS action");
 }
 
