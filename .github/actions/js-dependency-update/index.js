8522ae66aef9b62549259e3a2e59ab8582b573d5
Robin Snelson <rsnelson@heliostech.co.uk>
2026-05-17 12:42:26 +0100

    feat(17 - 2 - Custom Actions - JS) add logic for the workflow

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -58,18 +58,18 @@ async function run() {
 
   if (gitStatus.stdout.length > 0) {
     core.info("[js-dependency-update] : There are updates available!");
-    await exec.exec('git config --global user.name "gh-automation"');
-    await exec.exec('git config --global user.email "gh-automation@email.com"');
-    await exec.exec(`git checkout -b $targetBranch`, [], {
+    await exec.exec(`git config --global user.name "gh-automation"`);
+    await exec.exec(`git config --global user.email "gh-automation@email.com"`);
+    await exec.exec(`git checkout -b ${targetBranch}`, [], {
       ...commonExecOpts,
     });
-    await exec.exec("git add package.json package-lock.json", [], {
+    await exec.exec(`git add package.json package-lock.json`, [], {
       ...commonExecOpts,
     });
-    await exec.exec('git commit -m "chore: update dependencies"', [], {
+    await exec.exec(`git commit -m "chore: update dependencies"`, [], {
       ...commonExecOpts,
     });
-    await exec.exec("git push -u origin update-dependencies --force", [], {
+    await exec.exec(`git push -u origin ${targetBranch} --force`, [], {
       ...commonExecOpts,
     });
 
