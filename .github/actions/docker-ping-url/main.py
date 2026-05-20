497de13ad60c45f59a43df89c18b0119f0884658
Robin Snelson <rsnelson@heliostech.co.uk>
2026-05-20 19:43:18 +0100

    feat(17 - 3 - Custom Actions - Docker) use custom actions

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,18 +1,37 @@
 import os
+import requests
+import time
 
+def ping_url(url, delay, max_trials):
+    trials = 0
 
-def ping_url(url):
-    return True
-
-
+    while trials < max_trials:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Website {url} is reachable.")
+                return True
+        except: requests.ConnectionError
+            print(f"Website {url} is unreachable. REtrying in {delay} seconds...")
+            time.sleep(delay)
+            trials += 1
+        except: requests.exceptions.MissingSchema:
+            print(f"Invalid URL Format: {url} please provide a url with a valid schema")
+            return False
+        
+    return False
+  
 def run():
-    url = os.environ["INPUT_URL"]
+    website_url = os.getenv("INPUT_URL")
+    delay = int(os.getenv("INPUT_DELAY"))
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
 
-    result = ping_url(url)
+    website_reachable = ping_url(website_url,delay,max_trials)
 
-    if result is False:
-        raise Exception("Ping failed")
-
+    if not website_reachable:
+        raise Exception("Website {website_url} is malformed or unreachable")
+    
+    print(f" website {website_url} is reachable")
 
 if __name__ == "__main__":
     run()
