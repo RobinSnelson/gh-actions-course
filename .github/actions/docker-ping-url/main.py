94a7890ac4b17589dffe29a6a26cb11679e34679
Robin Snelson <rsnelson@heliostech.co.uk>
2026-05-20 19:18:58 +0100

    feat(17 - 3 - Custom Actions - Docker) use custom actions

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,19 @@
+import os
+
+
+def ping_url(url):
+    return True
+
+
+def run():
+    url = os.environ["INPUT_URL"]
+
+    result = ping_url(url)
+
+    if result is False:
+        raise Exception("Ping failed")
+
+
 if __name__ == "__main__":
-    print("Hello World")
+    run()
+
